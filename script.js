3ed89ef65041e3b78d007813b6684cbcf5cf5fa3
Nagarjuna Kocharla <nagarjunakocharla@Nagarjunas-MacBook-Pro.local>
2024-01-25 09:48:29 -0500

    added new icons

--- script.js
+++
@@ -1,5 +0,0 @@
-// This can be expanded based on specific interactive elements you wish to include
-document.querySelector('.contact-button').addEventListener('click', function() {
-    alert('Replace this with a link to your contact form or email!');
-});
-
